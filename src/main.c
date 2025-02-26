000e3afd7070dbe84de480da804dfb4da94560b4
vyacheslav <slavik.cola2@gmail.com>
2025-02-26 13:46:20 +0000

    small changes in processing events in proress_event()

--- src/main.c
+++ src/main.c
@@ -2,77 +2,87 @@
 #include "stdbool.h"
 #include "SDL2/SDL.h"
 
-SDL_Window* window = NULL;
-SDL_Renderer* renderer = NULL;
+SDL_Window *window = NULL;
+SDL_Renderer *renderer = NULL;
 bool is_running = false;
 
-bool initialize_window(void){
+bool initialize_window(void)
+{
 
-    if(SDL_Init(SDL_INIT_EVERYTHING)){
+    if (SDL_Init(SDL_INIT_EVERYTHING))
+    {
         fprintf(stderr, "Failed to initialize SDL!\n");
         return false;
     };
 
     window = SDL_CreateWindow(NULL, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_BORDERLESS);
-    if(!window){
-         fprintf(stderr, "Failed to create window!\n");
-         return false;
+    if (!window)
+    {
+        fprintf(stderr, "Failed to create window!\n");
+        return false;
     }
     renderer = SDL_CreateRenderer(window, -1, 0);
-    if(!renderer){
-          fprintf(stderr, "Failed to create renderer!\n");
-          return false;
+    if (!renderer)
+    {
+        fprintf(stderr, "Failed to create renderer!\n");
+        return false;
     }
 
     return true;
 }
 
-void setup(void){
-
+void setup(void)
+{
 }
 
-void process_input(void){
+void process_input(void)
+{
     SDL_Event event;
-    SDL_PollEvent(&event);
-  
-    switch (event.type)
+
+    while (SDL_PollEvent(&event))
     {
-    case SDL_QUIT:
-        is_running = false;
-        break;
-    case SDL_KEYDOWN:
-        if(event.key.keysym.sym == SDLK_ESCAPE){
+        switch (event.type)
+        {
+        case SDL_QUIT:
             is_running = false;
+            break;
+        case SDL_KEYDOWN:
+            if (event.key.keysym.sym == SDLK_ESCAPE)
+            {
+                is_running = false;
+            }
+            break;
+        default:
+            break;
         }
-        break;
-    default:
-        break;
     }
-
 }
 
-void update(void){
-
+void update(void)
+{
 }
 
-void render(void){
+void render(void)
+{
     SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
 
     SDL_RenderClear(renderer);
 
     SDL_RenderPresent(renderer);
 }
-int main(void){
+int main(void)
+{
     is_running = initialize_window();
 
     setup();
 
-    while(is_running){
+    while (is_running)
+    {
         process_input();
         update();
         render();
     }
-    
+
     printf("Renderer finished work!\n");
     return 0;
 }
